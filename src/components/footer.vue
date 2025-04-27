aa1e57a901f3538f9c9d06f7ff901b51cb533de9
Rakai Seto <mamangrakai@gmail.com>
2025-04-27 18:31:38 +0700

    fix(footer): benerin footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  <footer class="text-center pb-[0.75rem]" style="margin-top: 4rem;">Made by <a class="p-0" href="https://rakaiseto.cjom">Rakaiseto</a></footer>
+  <footer class="text-center pb-[0.75rem]" style="margin-top: 4rem;">
+    Made by <a class="p-0" href="https://rakaiseto.com" target="_blank">Rakaiseto</a>
+    <br>
+    Check it on <a class="p-0" href="https://github.com/RakaiSeto/panjaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaang" target="_blank">github</a>
+  </footer>
 </template>
 
 <style scoped></style>
